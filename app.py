78ccbfe2cb9ee48b6b6b217f0608b83c533cda97
Starlight-Nivi <159890768+Starlight-Nivi@users.noreply.github.com>
2024-11-25 10:10:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ def add(a, b):
     """Returns the sum of a and b."""
     return a + b
  
-if _name_ == "_main_":
+if _name_ == "_main_": 
     result = add(3, 5)
     print(f"The sum is: {result}")
